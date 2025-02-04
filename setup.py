3822d66b521d76c3a7a6a3e04689dfc728c8d2b5
andrelrocha <andrerocha0911@gmail.com>
2025-02-04 13:50:26 -0300

    deploy lib

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="grafosbiblioteca",
+    version="0.1", 
+    author="André Rocha",
+    author_email="andre.lucio@aluno.uece.br",
+    description="Uma biblioteca para manipulação de grafos usando lista de adjacência",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/andrelrocha/grafos-lib", 
+    packages=find_packages(),
+    install_requires=[
+        "numpy",  
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Teoria Grafos",
+    ],
+    python_requires=">=3.7",  
+)
